2857f068f224977b8b089ffe65b62ece233c2990
moray.grieve@me.com <you@example.com>
2016-11-06 20:36:19 +0000

    updates

--- netting/src/python/net.py
+++ netting/src/python/net.py
@@ -1,8 +1,8 @@
-import os, sys, random
+import random, math
 
+#constants for the run
 ACCOUNTS = ['Account A','Account B','Account C','Account D']
-CURRENCIES = ['AUD','CAD','CHF','CNH','EUR','GBP','HKD','JPY','NZD']
-
+CURRENCIES = ['AUD','CAD','CHF','CNH','EUR','GBP','HKD','JPY','NZD','PLN']
 PRICES = {
     'AUDUSD':(0.76689, 0.76705),
     'USDCAD':(1.33698,1.33713),
@@ -12,24 +12,59 @@ PRICES = {
     'GBPUSD':(1.23465,1.23485),
     'USDHKD':(7.75517,7.75538),
     'USDJPY':(102.61,102.62),
-    'NZDUSD':(0.73064,0.73084)
+    'NZDUSD':(0.73064,0.73084),
+    'USDPLN':(3.89141, 3.89354)
 }
+ORDERS = {}
+
+#data structures
+class FXOrder:
+    def __init__(self, base, terms, side, price, dealtAmount, dealtCurrency):
+        self.base = base
+        self.terms = terms
+        self.side = side
+        self.price = price
+        self.dealtAmount = dealtAmount
+        self.dealtCurrency = dealtCurrency
+
 
 def init():
+    """Populate the target positions randomly.
+    """
     target = {}
     for ccy in CURRENCIES:
         target[ccy]={}
         for acct in ACCOUNTS:
             r = random.randint(-5,5)
             if (r > 2):
-                ccy_amount = 1000000*random.randint(1,10)
-                target[ccy][acct]= (ccy_amount,exchange(ccy, ccy_amount))
+                ccy_amount = convertFromUSDMid(ccy, 1000000*random.randint(1,10))
+                target[ccy][acct]= (ccy_amount,convertToUSD(ccy, ccy_amount))
             elif (r < -2):
-                ccy_amount = -1000000*random.randint(1,10)
-                target[ccy][acct]= (ccy_amount,exchange(ccy, ccy_amount))
+                ccy_amount = convertFromUSDMid(ccy, -1000000*random.randint(1,10))
+                target[ccy][acct]= (ccy_amount,convertToUSD(ccy, ccy_amount))
     return target
 
-def exchange(ccy, amount):
+
+def roundup(amount):
+    """Roundup to the nearest million.
+    """
+    return int(math.ceil(amount/1000000.0)) * 1000000
+
+
+def convertFromUSDMid(ccy, amount):
+    """Convert from USD to target currency, using mid price.
+    """
+    if PRICES.has_key("USD"+ccy):
+        bid, ask = PRICES.get("USD"+ccy)
+        return roundup(amount * ((bid + ask)/2))
+    elif PRICES.has_key(ccy+"USD"):
+        bid, ask = PRICES.get(ccy+"USD")
+        return roundup(amount / ((bid + ask)/2))
+
+
+def convertToUSD(ccy, amount):
+    """Convert to USD from target currency, using bid or ask.
+    """
     if PRICES.has_key("USD"+ccy):
         bid, ask = PRICES.get("USD"+ccy)
         if (amount>0): return -1 * amount / bid
@@ -39,20 +74,24 @@ def exchange(ccy, amount):
         if (amount>0): return -1 * amount * ask
         else: return -1 * amount * bid
 
+
 def getHeader():
-    header = "| %-8s" % "CCY"
-    for act in ACCOUNTS: header = header + "| %-12s" % act + "  %+8s    " % "(USD)"
+    header = "| %-5s" % "CCY"
+    for act in ACCOUNTS: header = header + "| %-12s" % act
     return header+"|"
 
+
 def getRow(ccy, entries):
-    row = "| %-8s" % ccy
+    row = "| %-5s" % ccy
     for act in ACCOUNTS:
-        row = row + "| %s" % formatRowEntry(entries.get(act, (0,0))[0]) + "| %s" % formatRowEntry(entries.get(act, (0,0))[1])
+        row = row + "| %s" % formatRowEntry(entries.get(act, (0,0))[0])
     return row+"|"
+
 
 def formatRowEntry(value):
     if (value == 0): return "%12s"%"0"
-    return ("%12.5d"%value)
+    return ("%12d"%value)
+
 
 def printTarget():
     header = getHeader()
@@ -61,6 +100,7 @@ def printTarget():
     print "-"*len(header)
     for ccy in CURRENCIES: print getRow(ccy, target[ccy])
     print "-"*len(header)
+
 
 def printOrders():
     for ccy in CURRENCIES:
@@ -75,6 +115,8 @@ def printOrders():
                 if (amount[0]>0): print "BUY  %sUSD %f @ %f" % (ccy, amount[0], ask)
                 elif (amount[0]<0): print "SELL %sUSD %f @ %f" % (ccy, amount[0], bid)
 
+
+#the entry point
 if __name__ == "__main__":
     target = init()
     printTarget()
